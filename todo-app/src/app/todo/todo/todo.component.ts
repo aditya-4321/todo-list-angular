34ce6128f119166df05113756d81ebcf0d7b59c6
INTEL <Aditya Tomar>
2024-08-25 17:57:33 +0530

    Added initial logic to todo-component

--- todo-app/src/app/todo/todo/todo.component.ts
+++ todo-app/src/app/todo/todo/todo.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../models/todo';
+import { TodoService } from '../todo.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  constructor(private todoService: TodoService){}
+  private todoList: Todo[] = [];
+
+  ngOnInit(): void {
+    this.setTodoList();
+  }
+
+  setTodoList() {
+    this.todoService.getTodoList().subscribe((response: Todo[]) => {
+      if(response){
+        this.todoList = response;
+      }
+    })
+  }
+
+  getTodolist() : Todo[] {
+    return this.todoList;
+  }
 
 }
